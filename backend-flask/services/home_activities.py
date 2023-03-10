9c4f40f5cb416f486b61d67444710610ddf8f3e3
Shujath786 <shujath786@gmail.com>
2023-03-10 13:28:36 +0000

    changed attribute value

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -48,7 +48,7 @@ class HomeActivities:
       'replies': []
     }
     ]
-    span.set_attribute("app.results.length", len(results))
+    span.set_attribute("app.results_length", len(results))
     return results
 
     
