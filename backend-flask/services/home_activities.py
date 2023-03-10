3a5f17fe834cc7322553750819f10120359b492c
Shujath786 <shujath786@gmail.com>
2023-03-10 13:34:57 +0000

    changes

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -48,7 +48,7 @@ class HomeActivities:
       'replies': []
     }
     ]
-    span.set_attribute("app.results_length", len(results))
+    span.set_attribute("app.result_length", len(results))
     return results
 
     
