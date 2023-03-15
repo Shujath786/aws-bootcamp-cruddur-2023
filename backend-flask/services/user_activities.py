97882b0b0604af6fd2b056e6a1c9d559ede7b7e3
Shujath786 <shujath786@gmail.com>
2023-03-15 00:49:51 +0000

    temp commit

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -29,25 +29,5 @@ class UserActivities:
       }]
       model['data'] = results
       return model
-      model = {
-      'errors': None,
-      'data': None
-    }
-
-    now = datetime.now(timezone.utc).astimezone()
-
-    if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['blank_user_handle']
-    else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
-    return model
-
+      
     
