f8cddd1eb39ae0f4ea1ab3e9baad988c93ec2a67
Shujath786 <shujath786@gmail.com>
2023-03-15 00:46:11 +0000

    temp commit

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,7 +1,35 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-    model = {
+    # xray ---
+      segment = xray_recorder.begin_segment('user_activities')
+      model = {
+       'errors': None,
+       'data': None
+    }
+
+      now = datetime.now(timezone.utc).astimezone()
+      dict = {
+      "now": now.isoformat()
+    }
+    # xray ---
+      subsegment = xray_recorder.begin_subsegment('mock-data')
+      segment.put_metadata('key', dict, 'namespace')
+      if user_handle == None or len(user_handle) < 1:
+       model['errors'] = ['blank_user_handle']
+      else:
+       now = datetime.now()
+      results = [{
+        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'handle':  'Andrew Brown',
+        'message': 'Cloud is fun!',
+        'created_at': (now - timedelta(days=1)).isoformat(),
+        'expires_at': (now + timedelta(days=31)).isoformat()
+      }]
+      model['data'] = results
+      return model
+      model = {
       'errors': None,
       'data': None
     }
@@ -21,3 +49,5 @@ class UserActivities:
       }]
       model['data'] = results
     return model
+
+    
