6e4c5c9bbe0a6b1653bff91d08be2cefff9e509c
Shujath786 <shujath786@gmail.com>
2023-05-10 21:09:13 +0000

    post confirmation lambda

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -12,17 +12,17 @@ def lambda_handler(event, context):
         cur = conn.cursor()
         
         sql = f"""
-           "INSERT INTO users (
+           INSERT INTO public.users (
                 display_name, 
                 email,
                 handle, 
                 cognito_user_id
             )
             VALUES(
-                {user_display_name},
-                {user_email},
-                {user_handle},
-                {user_cognito_id})"
+                '{user_display_name}',
+                '{user_email}',
+                '{user_handle}',
+                '{user_cognito_id})'"
         """
         
         cur.execute(sql)
